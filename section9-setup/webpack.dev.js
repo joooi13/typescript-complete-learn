bd74b628ad98836fca6096b34bab3094dd8af933
joooi13 <joooi.y1313@gmail.com>
2021-04-22 09:22:37 +0900

    up to section10-142

---
+++ section9-setup/webpack.dev.js
@@ -0,0 +1,31 @@
+//pathはnodejsが内蔵しているデフォルトの機能
+const path = require('path');
+
+//nodejsのexport文
+//webpackコマンドを実行したときによまれる
+module.exports = {
+    mode: 'development',  //デフォルトはproduction
+    //スタート地点
+    //entrty: './dist/main.js',
+    entry: './src/food-app/main.ts',   //ts-loaderを使う場合こっち
+    //最終的にbundleしてアウトプットする
+    output: {
+        //キャッシュによって更新されない不具合を起こさないために
+        //filename:'[contenthash]bundle.js'
+        filename:'bundle.js',
+        path: path.resolve(__dirname,'dist'),    //絶対パスが入る
+        publicPath: '/dist/'
+    },
+    devtool: 'inline-source-map',
+    //import/exportできる。どのファイルをどう使うか
+    module: {
+        rules:[{
+            test: /\.ts$/,    //typescriptのファイルを対象とする
+            use: 'ts-loader',  
+            exclude: /node_modules/    //node_modulesからとってきたものだったらという意味。あんまり今回はいらない
+        }]
+    },
+    resolve: {
+        extensions :['.ts','.js']
+    }
+}
--- section9-setup/webpack.prod.js
+++ section9-setup/webpack.prod.js
@@ -1,20 +1,24 @@
 //pathはnodejsが内蔵しているデフォルトの機能
 const path = require('path');
 
+//不必要なjsファイルは削除してくれる
+const{ CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 //nodejsのexport文
 //webpackコマンドを実行したときによまれる
 module.exports = {
+    mode: 'production',  //デフォルトはproduction.
     //スタート地点
     //entrty: './dist/main.js',
-    entry: './src/food-app/main.ts',
+    entry: './src/food-app/main.ts',   //ts-loaderを使う場合こっち
     //最終的にbundleしてアウトプットする
     output: {
         //キャッシュによって更新されない不具合を起こさないために
         //filename:'[contenthash]bundle.js'
         filename:'bundle.js',
-        path: path.resolve(__dirname,'dist')    //絶対パスが入る
+        path: path.resolve(__dirname,'dist'),    //絶対パスが入る
     },
-    devtool: 'inline-source-map',
+    devtool: 'none',
     //import/exportできる。どのファイルをどう使うか
     module: {
         rules:[{
@@ -22,5 +26,11 @@ module.exports = {
             use: 'ts-loader',  
             exclude: /node_modules/    //node_modulesからとってきたものだったらという意味。あんまり今回はいらない
         }]
-    }
+    },
+    resolve: {
+        extensions :['.ts','.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 }
